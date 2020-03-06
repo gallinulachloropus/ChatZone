60241e428ed1f530d0c25c333817f49eafaab5d6
Nathan <n.parker31@outlook.com>
2020-03-06 13:55:43 -0500

    updated gitignore

---
+++ client/build/precache-manifest.9c705f36daddf30992acaeeca7750895.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f17169f5fd026686b16f884b7974fdbb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e91bd4f031845271b51f",
+    "url": "/static/css/main.7509bad5.chunk.css"
+  },
+  {
+    "revision": "adaa9ae3b721f1a879af",
+    "url": "/static/js/2.7322b6a7.chunk.js"
+  },
+  {
+    "revision": "1201c8656ff5734f581290e1df7d9b22",
+    "url": "/static/js/2.7322b6a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e91bd4f031845271b51f",
+    "url": "/static/js/main.4c034ade.chunk.js"
+  },
+  {
+    "revision": "9ecf16d4d4a427c4239e",
+    "url": "/static/js/runtime-main.f4995c64.js"
+  }
+]);
